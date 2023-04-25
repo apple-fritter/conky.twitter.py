5e6a86722f8e4b1299dc706403f38e02d798c6d1
@apple-fritter <125221355+apple-fritter@users.noreply.github.com>
2023-04-24 17:33:36 -0700

    Create twitter.py

---
+++ twitter.py
@@ -0,0 +1,24 @@
+import tweepy
+
+# Twitter API credentials
+consumer_key = 'your_consumer_key'
+consumer_secret = 'your_consumer_secret'
+access_token = 'your_access_token'
+access_token_secret = 'your_access_token_secret'
+
+# Authenticate with Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Retrieve notifications
+notifications = api.mentions_timeline(count=5)
+
+# Output notifications in Conky format
+output = ''
+for notification in notifications:
+    output += '${color #4F4F4F}${font Source Code Pro:size=10}'
+    output += '${goto 10}' + notification.author.name + ': '
+    output += '${goto 130}' + notification.text + '\n'
+    output += '${color}\n'
+print(output)
